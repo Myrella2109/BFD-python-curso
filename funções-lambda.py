5e590351e7c3c585ec11533257c331bf8ca5e4f9
Myrella2109 <mya.cl2109@gmail.com>
2025-10-02 21:48:04 +0000

    Exercicíos de funções lambda

--- funções-lambda.py
+++ funções-lambda.py
@@ -1,7 +1,33 @@
+from functools import reduce
+print("Exercicío 1-")
 lista = [1,2,3,4,5]
 print(list(map(lambda num: num*2, lista)))
 
 
+print("\nExercicío 2-")
 idade = [10, 15, 20, 25, 30]
-print("\n-",list(filter(lambda num: num % 2 == 0, idade)))
+print(list(filter(lambda num: num % 2 == 0, idade)))
 
+
+print("\nExercicío 3-")
+num = [1,2,3,4,5]
+print(reduce(lambda x, y: x + y, num))
+
+
+print("\nExercicío 4-")
+frutas = ["uva", "banana", "maçã", "laranja"]
+print(sorted(frutas, key=lambda x: len(x)))
+
+
+print("\nExercicío 5-")
+nom = ["ana", "pedro", "maria"]
+print(list(map(lambda x: x.capitalize(), nom)))
+
+print("\nExercicío 6-")
+nume = [1,2,3,4,5]
+print(reduce(lambda x, y: x*y, nume))
+
+
+print("\nExercicío 7-")
+frut = ["uva", "banana", "maçã", "laranja"]
+print(sorted(frut, key=lambda x: x[-1]))
